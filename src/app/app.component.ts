c5fa13a4dfbcdb119c99aff025f44f4f5b5bd4a5
ChanelMb37 <chanelmbianga@gmail.com>
2024-06-10 13:17:14 +0100

    commit two

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  template: `<h1>Welcome to {{ pokemons[1] }}!</h1> `,
+  template: `<h1>Welcome to {{ pokemonsList[1] }}!</h1> `,
 })
-export class AppComponent {
-  pokemons = ["Bulbizarre", "Salameche", "Carapuce"];
+export class AppComponent implements OnInit {
+  pokemonsList = ["Bulbizarre", "Salameche", "Carapuce"];
+
+  ngOnInit() {
+    console.table(this.pokemonsList);
+  }
 }
