c9bdbfa195975016589fed3c9a2f0c792f9193f6
ChanelMb37 <chanelmbianga@gmail.com>
2024-06-10 13:10:44 +0100

    commit one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  template: `<h1>Welcome to {{ title }}!</h1> `,
+  template: `<h1>Welcome to {{ pokemons[1] }}!</h1> `,
 })
 export class AppComponent {
-  title = "pokemon-app";
+  pokemons = ["Bulbizarre", "Salameche", "Carapuce"];
 }
