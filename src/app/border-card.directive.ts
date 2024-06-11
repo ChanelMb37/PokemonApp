2da56ae4fd9f658fa0bef22f223bc01b08747664
ChanelMb37 <chanelmbianga@gmail.com>
2024-06-11 15:01:49 +0100

    com five

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,23 +1,28 @@
-import { Directive, ElementRef, HostListener } from "@angular/core";
+import { Directive, ElementRef, HostListener, Input } from "@angular/core";
 
 @Directive({
   selector: "[pkmnBorderCard]", //cette directive s'sapplique sur les cartes des pokémons
 })
 export class BorderCardDirective {
+  private initialColor: string = "#f5f5f5";
+  private defaultColor: string = "#009688";
+  private defaultHeight: number = 180;
+
   constructor(private el: ElementRef) {
-    this.setBorder("#f5f5f5");
-    this.setHeight(180);
+    this.setBorder(this.initialColor);
+    this.setHeight(this.defaultHeight);
   }
+  @Input("pkmnBorderCard") borderColor: string; //alias
   //ecouter l'événement MouseEnter
   //lorske l'événement MouseEnter est appelé ,on modifie la couleur des bordures
   @HostListener("mouseenter") onMouseEnter() {
-    this.setBorder("009688");
+    this.setBorder(this.borderColor || this.defaultColor);
   }
 
   //ecouter l'événement MouseLeave
   //lorske l'utilisateur déplace son curseur en dehors d'un pokemon ,la couleur de la bordure repreend la valeur initiale :this.setBorder("#f5f5f5")
   @HostListener("mouseleave") onMouseLeave() {
-    this.setBorder("#f5f5f5");
+    this.setBorder(this.initialColor);
   }
 
   private setBorder(color: string) {
